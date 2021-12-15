5c4b00521fbd6216d4df7f0a091fc40f803d41a5
johnson-hung <jhcc.gamedev@gmail.com>
2021-12-15 11:34:22 -0600

    draw a simple map and player

--- raycasting.cpp
+++ raycasting.cpp
@@ -4,12 +4,13 @@
 #include <cstdint> // Fixed width integer types 
 #include <cassert> // Error handling
 
+// #define _BG_GRADIENT_
+
 // Turn (r, g, b, a) to 32-bit integer
 uint32_t packColor(const uint8_t r,
                    const uint8_t g, 
                    const uint8_t b, 
                    const uint8_t a = 255){
-    
     return (a << 24) + (b << 16) + (g << 8) + r;
 }
 
@@ -19,19 +20,17 @@ void unpackColor(const uint32_t& color,
                  uint8_t& g, 
                  uint8_t& b, 
                  uint8_t& a){
-    
     r = color & 255;
     g = (color >> 8) & 255;
     b = (color >> 16) & 255;
     a = (color >> 24) & 255;
 }
 
-// Write PPM image 
+// Write PPM image data to given file
 void generateImage(const std::string filename,
                    const std::vector<uint32_t>& image, 
                    const size_t w, 
                    const size_t h){
-
     assert(image.size() == w * h);
 
     std::ofstream ofs(filename, std::ios::binary); // Output stream to operate on file
@@ -46,22 +45,95 @@ void generateImage(const std::string filename,
     ofs.close();
 }
 
+void drawRectangle(std::vector<uint32_t>& img,
+                   const size_t winW,
+                   const size_t winH, 
+                   const size_t row, // Start from this row
+                   const size_t col, // Start from this col
+                   const size_t rectW,
+                   const size_t rectH,
+                   const uint32_t color){
+    assert(img.size() == winW * winH);
+
+    for (size_t offsetRow = 0; offsetRow < rectH; offsetRow++){
+        for (size_t offsetCol = 0; offsetCol < rectW; offsetCol++){
+            size_t r = row + offsetRow;
+            size_t c = col + offsetCol;
+            assert(r < winH && c < winW);
+            img[r * winW + c] = color;
+        }
+    }
+    
+}
+
 int main(){
     const size_t winW = 512; // Window width
     const size_t winH = 512; // Window height
     std::vector<uint32_t> img(winW * winH, 255); // Initialize the image to red
 
-    // Set gradient color
+    // Map properties
+    const size_t mapW = 16;
+    const size_t mapH = 16;
+    const char map[] = "0000000000000000"\
+                       "0              0"\
+                       "0              0"\
+                       "0              0"\
+                       "0    000000    0"\
+                       "0         0    0"\
+                       "0         0    0"\
+                       "0    0    0    0"\
+                       "0    0    0    0"\
+                       "0    0         0"\
+                       "0    0         0"\
+                       "0    000000    0"\
+                       "0              0"\
+                       "0              0"\
+                       "0              0"\
+                       "0000000000000000";
+
+    assert(sizeof(map) == mapW * mapH + 1);
+
+    // Player properties
+    float playerPosX = 7.5;
+    float playerPosY = 7.5;
+
+    // Set background color
     for (size_t row = 0; row < winW; row++){
         for (size_t col = 0; col < winW; col++){
+            #ifdef _BG_GRADIENT_
+            // Gradient color
             uint8_t r = 255 * (row / (float) winW);
             uint8_t g = 255 * (col / (float) winW);
             uint8_t b = 255 * ((row + col) / (float) (winW + winW));
+            #else
+            // Simple color (white)
+            uint8_t r = 255;
+            uint8_t g = 255;
+            uint8_t b = 255;
+            #endif
+
             img[row * winW + col] = packColor(r, g, b);
         }
     }
 
-    generateImage("./output.ppm", img, winW, winH); // Generate 24-bit color image (.ppm)
+    // Scale the map to window size and display it
+    const size_t rectW = winW / mapW;
+    const size_t rectH = winH / mapH;
+    for (size_t row = 0; row < mapH; row++){
+        for (size_t col = 0; col < mapW; col++){
+            if (map[row * mapW + col] == ' ') continue;
+            size_t r = row * rectH;
+            size_t c = col * rectW;
+            uint32_t color = packColor(0, 0, 0);
+            drawRectangle(img, winW, winH, r, c, rectW, rectH, color);
+        }
+    }
+
+    // Draw player on the map
+    drawRectangle(img, winW, winH, playerPosX * rectW, playerPosY * rectH, 5, 5, packColor(0, 0, 255));
+
+    // Generate 24-bit color image (.ppm)
+    generateImage("./output_2.ppm", img, winW, winH);
 
     return 0;
 }
