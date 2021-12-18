5d87855adebed2ddeb8e227a785719edb74fe44c
johnson-hung <jhcc.gamedev@gmail.com>
2021-12-18 17:57:36 -0600

    render wall textures

--- raycasting.cpp
+++ raycasting.cpp
@@ -80,6 +80,7 @@ bool loadTexture(const std::string filename,
     int w;
     int h;
     unsigned char* img = stbi_load(filename.c_str(), &w, &h, &numChannels, 0);
+
     if (!img){
         std::cerr << "Error: Cannot load textures" << std::endl;
         return false;
@@ -92,6 +93,7 @@ bool loadTexture(const std::string filename,
 
     count = w / h;
     size = w / count;
+    std::cout << "Count of textures = " << count << "; size of texture = " << size << std::endl;
     if (w != h * int(count)){
         std::cerr << "Error: Textures are not packed horizontally" << std::endl;
         stbi_image_free(img);
@@ -112,6 +114,26 @@ bool loadTexture(const std::string filename,
     return true;
 }
 
+// Return texture data at the specific column
+std::vector<uint32_t> getTextureColumn(const std::vector<uint32_t>& texture,
+                                       const size_t textureCount,
+                                       const size_t textureSize,
+                                       const size_t textureIdx,
+                                       const size_t start,
+                                       const size_t colH){
+    const size_t w = textureSize * textureCount;
+    const size_t h = textureSize;
+    assert(texture.size() == w * h && start < textureSize && textureIdx < textureCount);
+
+    std::vector<uint32_t> col(colH);
+    for (size_t y = 0; y < colH; y++){   
+        size_t tx = textureIdx * textureSize + start;
+        size_t ty = textureSize * (y / (float) colH);
+        col[y] = texture[ty * w + tx];
+    }
+    return col;
+}
+
 int main(){
     const size_t winW = 1024; // Window width (top-down map and 3D view)
     const size_t winH = 512; // Window height
@@ -120,22 +142,22 @@ int main(){
     // Map properties
     const size_t mapW = 16;
     const size_t mapH = 16;
-    const char map[] = "0000000000000000"\
-                       "0              0"\
-                       "6              0"\
-                       "6              0"\
-                       "6     22200    0"\
-                       "0  0      0    0"\
-                       "0         0    0"\
-                       "0    0    1    0"\
-                       "0    3    1    0"\
-                       "0    0         0"\
-                       "0    0         5"\
-                       "0    00440     5"\
-                       "0              5"\
-                       "0            000"\
-                       "0              0"\
-                       "0000000000000000";
+    const char map[] = "0111111111111111"\
+                       "0              1"\
+                       "4              1"\
+                       "4              1"\
+                       "4     22211    1"\
+                       "1  0      1    1"\
+                       "1         1    0"\
+                       "0    1    1    0"\
+                       "0    3    1    1"\
+                       "1    1         1"\
+                       "1    1         5"\
+                       "1    11441     5"\
+                       "1              5"\
+                       "1            333"\
+                       "1              1"\
+                       "1111111111111111";
 
     assert(sizeof(map) == mapW * mapH + 1);
 
@@ -144,13 +166,6 @@ int main(){
     float playerPosY = 7.5;
     float playerRot = 45 * PI / 180; // Rotation
     float playerFov = PI / 3; // Field of view
-
-
-    const size_t numColors = 10;
-    std::vector<uint32_t> colors(numColors);
-    for (size_t i = 0; i < numColors; i++){
-        colors[i] = packColor(rand() % 255, rand() % 255, rand() % 255);
-    }
 
     std::vector<uint32_t> texture;
     size_t textureSize;
@@ -189,9 +204,9 @@ int main(){
             if (map[y * mapW + x] == ' ') continue;
             size_t imgX = x * rectW;
             size_t imgY = y * rectH;
-            size_t colorIdx = map[y * mapW + x] - '0';
-            assert(colorIdx < numColors);
-            drawRectangle(img, winW, winH, imgX, imgY, rectW, rectH, colors[colorIdx]);
+            size_t textureIdx = map[y * mapW + x] - '0';
+            assert(textureIdx < textureCount);
+            drawRectangle(img, winW, winH, imgX, imgY, rectW, rectH, texture[textureIdx*textureSize]);
         }
     }
 
@@ -207,30 +222,41 @@ int main(){
         for (float dist = 0; dist < 23; dist += 0.01){
             float targetX = playerPosX + dist * cos(rotation);
             float targetY = playerPosY + dist * sin(rotation);
+            int x = targetX * rectW;
+            int y = targetY * rectH;
 
-            size_t x = targetX * rectW;
-            size_t y = targetY * rectH;
+            // FOV part
             img[y*winW + x] = color;
 
+            // Ray hits a block, render vertical column for 3D view
             if (map[(int) targetY * mapW + (int) targetX] != ' '){
-                // Ray hits a block, render vertical column for 3D view
                 size_t h = winH / (dist * cos(rotation - playerRot)); // Fix fisheye distortion
-                size_t colorIdx = map[(int)targetY * mapW + (int)targetX] - '0';
-                assert(colorIdx < numColors);
-                drawRectangle(img, winW, winH, winW/2 + i, winH/2 - h/2, 1, h, colors[colorIdx]);
+                size_t textureIdx = map[(int)targetY * mapW + (int)targetX] - '0';
+                assert(textureIdx < textureCount);
+                
+                // Get fractional part of targetX and targetY to determine the 
+                // position of the hitpoint (top/bottom or left/right side of block)
+                float hitX = targetX - floor(targetX + 0.5); // ~0 -> hit left/right side of block
+                float hitY = targetY - floor(targetY + 0.5); // ~0 -> hit top/bottom side of block
+                int start = std::abs(hitY) > std::abs(hitX)? hitY * textureSize : hitX * textureSize;
+                if (start < 0) start += textureSize; // Make sure start position is positive
+                assert(start >= 0 && start < (int) textureSize);
+
+                // Write the specific column
+                std::vector<uint32_t> col = getTextureColumn(texture, textureCount, textureSize,
+                                                             textureIdx, start, h);
+                x = winW/2 + i;
+                for (size_t j = 0; j < h; j++){
+                    y = winH/2 - h/2 + j;
+                    if (x < 0 || y >= (int) winH) continue;
+                    img[y*winW + x] = col[j];
+                }
                 break;
             }
         }
     }
-    
-    const size_t tId = 3;
-    for (size_t x = 0; x < textureSize; x++){
-        for (size_t y = 0; y < textureSize; y++){
-            img[y*winW + x] = texture[y*textureCount*textureSize + tId*textureSize + x];
-        }
-    }
 
     // Generate 24-bit color image (.ppm)
-    generateImage("./img/output_6.ppm", img, winW, winH);
+    generateImage("./img/output_7.ppm", img, winW, winH);
     return 0;
 }
