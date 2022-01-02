5c1bfa003651832c529a5bda91537dbf4e78268d
johnson-hung <jhcc.gamedev@gmail.com>
2022-01-02 15:57:03 -0600

    modify player constructor

--- src/player.cpp
+++ src/player.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "player.h"
+#include "settings.h"
 
 Player::Player(){
+    x = PLAYER_DEFAULT_X;
+    y = PLAYER_DEFAULT_Y;
+    rot = 0;
+    fov = PLAYER_DEFAULT_FOV;
     walk = 0;
     turn = 0;
 }
@@ -11,8 +16,10 @@ Player::Player(float pX, float pY, float pRot, float pFov){
     y = pY;
     rot = pRot;
     fov = pFov;
+    walk = 0;
+    turn = 0;
 }
 
 void Player::printPlayerPosition(){
-    std::cout << "Player: ( " << x <<", " << y << ")" << std::endl;
+    std::cout << "Player: (" << x <<", " << y << ")" << std::endl;
 }
