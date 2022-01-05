cba7f94a194d14327358ca03d459332fd07231f4
johnson-hung <jhcc.gamedev@gmail.com>
2022-01-05 17:25:43 -0600

    add comments to gamestate

--- include/game_state.h
+++ include/game_state.h
@@ -8,10 +8,19 @@ class GameState{
         GameState(){}
 
     public:
+        // Set up the world for the current game state
         virtual void init(Game* game) = 0;
+
+        // Handle events (e.g. user inputs)
         virtual void handleEvents(Game* game) = 0;
+
+        // Method to update the world under the current game state
         virtual void update(Game* game) = 0;
+
+        // Method to render the world under the current game state
         virtual void render(Game* game) = 0;
+
+        // Pass new state to game manager to switch state
         void changeState(Game* game, GameState* newState){
             game->changeState(newState);
         }
