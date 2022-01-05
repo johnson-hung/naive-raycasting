2acabbf9585a61d4f3a6b3a5434c61a2455407d1
johnson-hung <jhcc.gamedev@gmail.com>
2022-01-05 17:38:22 -0600

    add comments to gamestate terminate header and cpp

--- include/state_terminate.h
+++ include/state_terminate.h
@@ -5,17 +5,25 @@
 
 class StateTerminate: public GameState{
     private:
-        static StateTerminate i_StateTerminate;
+        static StateTerminate i_StateTerminate; // Singleton
 
     protected:
         StateTerminate(){}
 
     public:
+        // Set up the world or canvas display for the current game state
         void init(Game* game);
+
+        // Handle events (e.g. user inputs)
         void handleEvents(Game* game);
+
+        // Method to update the world under the current game state
         void update(Game* game);
+        
+        // Method to render the world under the current game state
         void render(Game* game);
 
+        // Return pointer to the singleton of terminate state
         static StateTerminate* getInstance();
 
         ~StateTerminate(){}
--- src/state_terminate.cpp
+++ src/state_terminate.cpp
@@ -3,6 +3,7 @@
 
 StateTerminate StateTerminate::i_StateTerminate;
 
+// Set up the world or canvas display for the current game state
 void StateTerminate::init(Game* game){
     std::cout<<"[Game] State: Terminate"<<std::endl;
     game->textRect.w = 205;
@@ -10,10 +11,14 @@ void StateTerminate::init(Game* game){
     game->terminate();
 }
 
+// Handle events (e.g. user inputs)
 void StateTerminate::handleEvents(Game* game){}
 
+// Method to update the world under the current game state
 void StateTerminate::update(Game* game){}
 
+// Method to render the world under the current game state
 void StateTerminate::render(Game* game){}
 
+// Return pointer to the singleton of terminate state
 StateTerminate* StateTerminate::getInstance(){ return &i_StateTerminate; }
